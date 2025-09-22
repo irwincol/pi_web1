cad696515b381b8fca05e3645e102422b77e4b21
Estiben Manco <estibenmanco1031@gmail.com>
2025-09-22 10:38:13 -0500

    se agregaron las actividades en la pagina de estudiantes

--- cronoClase-app/src/scripts/MostrarTareas.js
+++ cronoClase-app/src/scripts/MostrarTareas.js
@@ -1,18 +1,34 @@
-const contenido = document.getElementById("actividadAPI");
+async function obtenerUsuarios(contenedorId) {
+  const contenido = document.getElementById(contenedorId);
 
-async function obtenerUsuarios() {
-  const contenido = document.getElementById("actividadAPI");
-
-  fetch("http://localhost:3001/tareas")
+ fetch("http://localhost:3001/tareas")
     .then((response) => response.json())
-
     .then((data) => {
-      const lista = document.getElementById("actividadAPI");
+      contenido.innerHTML = ""; // Limpiar antes de agregar
       data.forEach((tarea) => {
         //contenido.innerHTML += `<p>${tarea.titulo} - entrega: ${tarea.fecha_entrega}</p>`
 
         contenido.innerHTML += `<div class="actividad">
-         
+                  <strong> ${tarea.titulo} <img src="img/quizas.png" alt=""></strong>
+                  <p>Fecha: ${tarea.fecha_entrega}</p>
+                  <div class="estado-info">
+                    <span class="estado pendiente">Pendiente</span>
+                  </div>`;
+      });
+    });
+}
+
+    async function obtenerUsuarios(contenedorId) {
+  const contenido = document.getElementById(contenedorId);
+
+  fetch("http://localhost:3001/mat2")
+    .then((response) => response.json())
+    .then((data) => {
+      contenido.innerHTML = ""; // Limpiar antes de agregar
+      data.forEach((tarea) => {
+        //contenido.innerHTML += `<p>${tarea.titulo} - entrega: ${tarea.fecha_entrega}</p>`
+
+        contenido.innerHTML += `<div class="actividad">
                   <strong> ${tarea.titulo} <img src="img/quizas.png" alt=""></strong>
                   <p>Fecha: ${tarea.fecha_entrega}</p>
                   <div class="estado-info">
